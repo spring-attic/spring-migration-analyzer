93dcbb6f4880e799abcb0fa008cef9d781174267
Andy Wilkinson <wilkinsona@vmware.com>
2011-12-09 12:54:40 +0000

    [SMA-1] Add support for scanning mutiple archives at a time
    The inputPath may now point to a single archive or to a directory. In the event
    of it pointing to a directory the directory structure is scanned and every
    archive that's found is analyzed. An archive is identified by the file being in
    Zip format, i.e. there's no requirement for an archive to have a well-known
    file suffix for it to be analyzed.
    
    A separate report is produced for each archive that's analyzed. Each report is
    written into its own directory beneath the configured outputPath location. In
    the case of multiple archives being scanned, the directory structure of the
    reports that are produced will mirror the structure of the directory that was
    provided as input.

---
+++ command-line/src/main/java/org/springframework/migrationanalyzer/commandline/ZipArchiveDiscoverer.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.migrationanalyzer.commandline;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.migrationanalyzer.util.IoUtils;
+import org.springframework.migrationanalyzer.util.ZipUtils;
+
+final class ZipArchiveDiscoverer implements ArchiveDiscoverer {
+
+    private final Logger logger = LoggerFactory.getLogger(ZipArchiveDiscoverer.class);
+
+    @Override
+    public List<File> discover(File location) {
+        List<File> discovered = new ArrayList<File>();
+        doDiscover(location, discovered);
+        return discovered;
+    }
+
+    private void doDiscover(File candidate, List<File> discovered) {
+        if (candidate.isDirectory()) {
+            for (File inDirectory : candidate.listFiles()) {
+                doDiscover(inDirectory, discovered);
+            }
+        } else {
+            FileInputStream in = null;
+            try {
+                in = new FileInputStream(candidate);
+                if (ZipUtils.isZipFile(in)) {
+                    discovered.add(candidate);
+                }
+            } catch (IOException ioe) {
+                this.logger.warn("Unable to examine candidate archive '{}', it will not be analyzed", candidate);
+            } finally {
+                IoUtils.closeQuietly(in);
+            }
+        }
+    }
+}
